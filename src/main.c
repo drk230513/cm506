6346e5c2bd40a195a9dcb3ed51a0ca289a523ee8
DavidKendall <david.kendall@northumbria.ac.uk>
2015-08-23 11:09:27 +0100

    Better approach to pin configuration

--- src/main.c
+++ src/main.c
@@ -20,8 +20,8 @@
 void delay(uint32_t ms);
 
 int main() {
-	IOCON_P1_13 = 0;
-	IOCON_P1_18 = 0;
+	IOCON_P1_13 &= ~0x1FUL;
+	IOCON_P1_18 &= ~0x1FUL;
 	GPIO1DIR |= (LED1PIN | LED3PIN);
 	while (true) {
 		GPIO1PIN |= (LED1PIN | LED3PIN);
