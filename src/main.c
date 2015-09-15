bc93169c89be5b5b72e191c3f82a14461f5d4064
DavidKendall <david.kendall@northumbria.ac.uk>
2015-09-15 17:32:42 +0100

    Cycle through the colours on the RGB LED

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 /*
- * @brief A program for the EA NXP LPC4088QSB to flash LED1, LED2, LED3 and LED4.
+ * @brief A program for the EA NXP LPC4088QSB to flash LED1, LED2, LED3 and LED4
+          and the RGB LED.
  * @author David Kendall
  * @date July 2015
  */
@@ -9,6 +10,9 @@
 
 #define IOCON_P1_18 (*(volatile uint32_t *)(0x4002C0C8UL))   
 #define IOCON_P0_13 (*(volatile uint32_t *)(0x4002C034UL))
+#define IOCON_P1_5  (*(volatile uint32_t *)(0x4002C094UL))
+#define IOCON_P1_7  (*(volatile uint32_t *)(0x4002C09CUL))
+#define IOCON_P1_11 (*(volatile uint32_t *)(0x4002C0ACUL))
 #define IOCON_P1_13 (*(volatile uint32_t *)(0x4002C0B4UL))
 #define IOCON_P2_19 (*(volatile uint32_t *)(0x4002C14CUL))   
 #define GPIOBASE   (0x20098000UL)
@@ -30,28 +34,65 @@
 #define LED2PIN    (1UL << 13)
 #define LED3PIN    (1UL << 13)
 #define LED4PIN    (1UL << 19)
+#define LEDRPIN    (1UL << 11)
+#define LEDGPIN    (1UL << 5)
+#define LEDBPIN    (1UL << 7)
 
 void delay(uint32_t ms);
 
 int main() {
+	uint32_t rgbState = 0;
+	
 	IOCON_P1_18 &= ~0x1FUL;
 	IOCON_P0_13 &= ~0x1FUL;
 	IOCON_P1_13 &= ~0x1FUL;
 	IOCON_P2_19 &= ~0x1FUL;
+	IOCON_P1_5 &=  ~0x1FUL;
+	IOCON_P1_7 &=  ~0x1FUL;
+	IOCON_P1_11 &= ~0x1FUL;
 	GPIO1DIR |= LED1PIN;
 	GPIO0DIR |= LED2PIN;
 	GPIO1DIR |= LED3PIN;
 	GPIO2DIR |= LED4PIN;
+	GPIO1DIR |= LEDRPIN;
+	GPIO1DIR |= LEDGPIN;
+	GPIO1DIR |= LEDBPIN;
 	while (true) {
-		GPIO1SET = LED1PIN;
-		GPIO0SET = LED2PIN;
+		GPIO1CLR = LED1PIN;
+		GPIO0CLR = LED2PIN;
 		GPIO1SET = LED3PIN;
 		GPIO2SET = LED4PIN;
 		delay(1000);
-		GPIO1CLR = LED1PIN;
-		GPIO0CLR = LED2PIN;
+		GPIO1SET = LED1PIN;
+		GPIO0SET = LED2PIN;
 		GPIO1CLR = LED3PIN;
 		GPIO2CLR = LED4PIN;
+		switch (rgbState) {
+			case 0: {
+				GPIO1CLR = LEDRPIN;
+				GPIO1SET = LEDGPIN | LEDBPIN;
+				rgbState = 1;
+				break;
+			}
+			case 1: {
+				GPIO1CLR = LEDGPIN;
+				GPIO1SET = LEDRPIN | LEDBPIN;
+				rgbState = 2;
+				break;
+			}
+			case 2: {
+				GPIO1CLR = LEDBPIN;
+				GPIO1SET = LEDRPIN | LEDGPIN;
+				rgbState = 0;
+				break;
+			}
+			default: {
+				GPIO1CLR = LEDRPIN;
+				GPIO1SET = LEDGPIN | LEDBPIN;
+				rgbState = 1;
+				break;
+			}
+		}
 		delay(1000);
 	}
 }
